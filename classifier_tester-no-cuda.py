2b56319f0c08fcdfa26db1764d56624c0fbf9b6f
edinpeter <edin.3@osu.edu>
2018-02-24 21:06:58 -0500

    Added un-cuda'd code

---
+++ classifier_tester-no-cuda.py
@@ -0,0 +1,68 @@
+import torch
+from dice_dataset import DiceDataset
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import time
+import random
+
+class Net(nn.Module):
+    def __init__(self):
+    	self.network_width = 10
+        self.mystery_1 = 18
+        self.mystery_2 = 20
+        self.mystery_3 = 20
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, self.network_width, 10)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(self.network_width, self.mystery_1, 5)
+        self.fc1 = nn.Linear(self.mystery_1 * self.mystery_2 * self.mystery_3, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, self.mystery_1 * self.mystery_2 * self.mystery_3)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+testset = DiceDataset("./dice_small", "/home/peter/Desktop/dice/randoms_small/", train=False, no_dice_max=400, train_percent=0.5)
+
+model = torch.load('classifier-no-cuda31703.pt')
+print "Dice: ", testset.len_dice()
+print "No dice: ", testset.len_no_dice()
+
+start = time.time()
+for i in range(0, 1000):
+    r1 = random.randint(0, testset.len_no_dice() + testset.len_dice() + -1)
+    r2 = random.randint(0, testset.len_no_dice() + testset.len_dice() + -1)
+    r3 = random.randint(0, testset.len_no_dice() + testset.len_dice() + -1)
+    r4 = random.randint(0, testset.len_no_dice() + testset.len_dice() + -1)
+
+    print r1, r2, r3, r4, testset.len_no_dice() + testset.len_dice() + 1
+    t = torch.cat((testset[r1][0].unsqueeze(0), testset[r2][0].unsqueeze(0), testset[r3][0].unsqueeze(0), testset[r4][0].unsqueeze(0)), dim=0)
+
+    soft = nn.Softmax(1)
+    optim = Variable(t)
+    outputs = model(optim)
+    print "Testing..."
+    _, predicted = torch.max(outputs.data, 1)
+
+    print outputs.data
+    print soft(Variable(outputs.data))
+    print outputs.data[1]
+
+    softed = soft(Variable(outputs.data))
+    print "\n\n"
+    for i in range(0,4):
+    	print "Confidence ", softed[i][predicted[i]]
+    	print "Prediction: ", predicted[i]
+    	print "\n\n"
+    #end = time.clock()
+
+end = time.time()
+print "Completed in : ", end - start
---
+++ dice-classifier-no-cuda.py
@@ -0,0 +1,127 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from dice_dataset import DiceDataset
+import matplotlib.pyplot as plt
+import random
+
+trainset = DiceDataset("/home/peter/Desktop/dice/dice_small", "/home/peter/Desktop/dice/randoms_small/", train=True, no_dice_max=70, train_percent=0.5)
+
+testset = DiceDataset("/home/peter/Desktop/dice/dice_small", "/home/peter/Desktop/dice/randoms_small/", train=False, no_dice_max=30, train_percent=0.5)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=4)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=True, num_workers=4)
+
+classes = ('no-dice', 'dice')
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+    	self.network_width = 10
+        self.mystery_1 = 18
+        self.mystery_2 = 20
+        self.mystery_3 = 20
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, self.network_width, 10)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(self.network_width, self.mystery_1, 5)
+        self.fc1 = nn.Linear(self.mystery_1 * self.mystery_2 * self.mystery_3, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, self.mystery_1 * self.mystery_2 * self.mystery_3)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(20):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels, filenames = data
+
+        # wrap them in Variable
+        #print (inputs[0])
+        inputs, labels = Variable(inputs), Variable(labels)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.data[0]
+        if i % 50 == 0:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+dataiter = iter(testloader)
+images, labels, filenames = dataiter.next()
+
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+outputs = net(Variable(images))
+
+_, predicted = torch.max(outputs.data, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+correct = 0
+total = 0
+
+for data in testloader:
+    images, labels, filenames = data
+    outputs = net(Variable(images))
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+
+print('Accuracy of the network on the %i test images: %d %%' %  (len(testset), (100 * correct / total)))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+for data in testloader:
+    images, labels, filenames = data
+    outputs = net(Variable(images))
+    _, predicted = torch.max(outputs.data, 1)
+    c = (predicted == labels).squeeze()
+    #print labels
+    for i in range(len(labels)):
+        label = labels[i]
+        class_correct[label] += c[i]
+        class_total[label] += 1
+
+
+for i in range(2):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
+
+r = random.randint(0,100000)
+torch.save(net, 'classifier-no-cuda'+str(r)+'.pt')
